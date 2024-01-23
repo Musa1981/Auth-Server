3245cd157a7b6f034bcee7bc20ba3b957860168a
Musa Vakilov <dino_crizis@yahoo.com>
2024-01-23 11:39:20 +0100

    lagt till git ignore

--- src/server.js
+++ src/server.js
@@ -3,12 +3,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth_routes');
 const cors = require('cors'); 
-
+const helmet = require('helmet');
 const app = express();
 
 app.use(cors());  // Use CORS middleware without any restrictions
 app.use(bodyParser.json());
 app.use('/auth', authRoutes);
+app.use(helmet());
 
 module.exports = app;
 
